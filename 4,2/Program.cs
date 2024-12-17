08061a4ce5550c89e992d5d5c716d50d837adb50
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-12-17 14:03:42 +0100

    Add project files.

---
+++ 4,2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uppgift_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = -1;
+            while (number != 0)
+            {
+                Console.Write("Skriv in et heltal (0 för att avsluta): ");
+
+               
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number != 0)
+                    {
+                        Console.WriteLine($"Du skrev in: {number}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du skrev in 0. Programmet avslutas.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltig input. Skriv in ett giltigt heltal.");
+                }
+            }
+        }
+    }
+}
